df8cf3f01f8708df2b7fbb5629d7e6679ad685cd
Mahmoud El-Shawa <moshawa0@gmail.com>
2022-05-17 17:41:55 -0400

    fixed accordion in-out

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,28 +1,46 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { useTransition, animated } from 'react-spring'
 
 export default function Form(props) {
+    const [open, setOpen] = useState(false)
+    const animateDetails = useTransition(open, {
+        from: { opacity: 0, maxHeight: '0' },
+        enter: { opacity: 1, maxHeight: '300px' },
+        leave: { opacity: 0, maxHeight: '0' }
+    })
+    const detailsRef = useRef()
+
+    const handleSummary = (e) => {
+        e.preventDefault()
+        setOpen(!open)
+    }
     return (
         <>
             <h1>Robot AMA</h1>
             <form onSubmit={props.handleSubmit}>
                 <textarea placeholder='Write prompt here...' onChange={props.handleChange} value={props.prompt.prompt} name="prompt" id="text" cols="30" rows="4"></textarea>
-                <details>
-                    <summary>Super Secret Settings</summary>
-                    <select onChange={props.handleChange} value={props.prompt.model} name="model" >
-                        <option value=''>Pick an engine</option>
-                        <option value='text-curie-001'>text-curie-001 (default)</option>
-                        <option value='text-ada-001'>text-ada-001 (faster)</option>
-                        <option value='text-davinci-002'>text-davinci-002 (more sophisticated)</option>
-                    </select>
+                <details open>
+                    <summary onClick={handleSummary}>Super Secret Settings</summary>
+                    {animateDetails((style, isOpen) => (
+                        isOpen ? <animated.div style={style}>
+                            <div className="detailsContent">
+                                <select onChange={props.handleChange} value={props.prompt.model} name="model" >
+                                    <option value=''>Pick an engine</option>
+                                    <option value='text-curie-001'>text-curie-001 (default)</option>
+                                    <option value='text-ada-001'>text-ada-001 (faster)</option>
+                                    <option value='text-davinci-002'>text-davinci-002 (more sophisticated)</option>
+                                </select>
+                                <hr />
+                                <label htmlFor="temperature">Temperature (creativity)</label>
+                                <input onChange={props.handleChange} value={props.prompt.temperature} type="range" name="temperature" defaultValue={0.5} step={0.01} min={0} max={1} />
+                                <hr />
+                                <label htmlFor="maxTokens">Max tokens (response length)</label>
+                                <input onChange={props.handleChange} value={props.prompt.maxTokens} type="range" defaultValue={50} name="max_tokens" min={20} max={200} />
+                            </div>
+                        </animated.div>
+                            : null
+                    ))}
 
-                    <label htmlFor="temperature">Temperature (creativity)</label>
-                    <input onChange={props.handleChange} value={props.prompt.temperature} type="range" name="temperature" defaultValue={0.5} step={0.01} min={0} max={1} />
-                    <div className="form-control">
-
-                        <label htmlFor="maxTokens">Max tokens</label>
-                        <input onChange={props.handleChange} value={props.prompt.maxTokens} type="range" defaultValue={50} name="max_tokens" min={20} max={200} />
-
-                    </div>
                 </details>
                 <button type="submit" >Submit</button>
             </form>
