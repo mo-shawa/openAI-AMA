3929f1a39af0750097d753257fcf06a812db21ef
moshawa <moshawa0@gmail.com>
2023-02-12 00:15:39 -0500

    persist settings + start visual update

--- src/App.js
+++ src/App.js
@@ -1,98 +1,129 @@
-import './App.css';
-import Card from './components/Card/Card';
-import Form from './components/Form/Form';
-import LoadingIcon from './components/LoadingIcon/LoadingIcon';
-import IconBar from './components/IconBar/IconBar';
-import { useState, useEffect } from 'react';
-import { useTransition, animated } from 'react-spring'
-
-
+import "./App.css"
+import Card from "./components/Card/Card"
+import Form from "./components/Form/Form"
+import LoadingIcon from "./components/LoadingIcon/LoadingIcon"
+import IconBar from "./components/IconBar/IconBar"
+import { useState, useEffect } from "react"
+import { useTransition, animated } from "react-spring"
 
 function App() {
-  const [prompt, setPrompt] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [responses, setResponses] = useState(() => {
-    const storage = JSON.parse(localStorage.getItem('responses'));
-    return storage || [];
-  });
-  const [settings, setSettings] = useState({
-    model: "text-curie-001",
-    temperature: 0.5,
-    max_tokens: 50
-  })
+	const [prompt, setPrompt] = useState("")
+	const [isLoading, setIsLoading] = useState(false)
+	const [responses, setResponses] = useState(() => {
+		const storage = JSON.parse(localStorage.getItem("responses"))
+		return storage || []
+	})
+	const [settings, setSettings] = useState(() => {
+		const localSettings = JSON.parse(localStorage.getItem("settings"))
 
-  useEffect(() => {
-    localStorage.setItem('responses', JSON.stringify(responses));
-  }, [responses])
+		return (
+			localSettings || {
+				model: "text-curie-001",
+				temperature: 0.5,
+				max_tokens: 50,
+			}
+		)
+	})
 
+	useEffect(() => {
+		localStorage.setItem("responses", JSON.stringify(responses))
+	}, [responses])
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
+	useEffect(() => {
+		localStorage.setItem("settings", JSON.stringify(settings))
+	}, [settings])
 
-    const body = JSON.stringify({
-      prompt: prompt,
-      model: settings.model,
-      temperature: settings.temperature,
-      max_tokens: settings.max_tokens
-    })
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		setIsLoading(true)
 
-    console.log(body)
+		const body = JSON.stringify({
+			prompt: prompt,
+			model: settings.model,
+			temperature: settings.temperature,
+			max_tokens: settings.max_tokens,
+		})
 
-    const response = await fetch('/api', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: body
-    })
-    const data = await response.json();
-    setResponses([{
-      prompt: prompt,
-      response: data.response.choices[0].text,
-      model: data.response.model
-    }, ...responses]);
-    setIsLoading(false);
-    setPrompt('');
-  }
+		console.log(body)
 
-  const transitionIcon = useTransition(isLoading, {
-    from: { opacity: 0, },
-    enter: { opacity: 1 },
-    leave: { opacity: 0, },
-  })
+		const response = await fetch("/api", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: body,
+		})
+		const data = await response.json()
+		setResponses([
+			{
+				prompt: prompt,
+				response: data.response.choices[0].text,
+				model: data.response.model,
+			},
+			...responses,
+		])
+		setIsLoading(false)
+		setPrompt("")
+	}
 
-  const transitionCard = useTransition(!isLoading, {
-    from: { opacity: 0, marginTop: 100 },
-    enter: { opacity: 1, marginTop: 0 },
-    leave: { opacity: 0, marginTop: 100 },
-  })
+	const transitionIcon = useTransition(isLoading, {
+		from: { opacity: 0 },
+		enter: { opacity: 1 },
+		leave: { opacity: 0 },
+	})
 
+	const transitionCard = useTransition(!isLoading, {
+		from: { opacity: 0, marginTop: 100 },
+		enter: { opacity: 1, marginTop: 0 },
+		leave: { opacity: 0, marginTop: 100 },
+	})
 
-  return (
-    <div className="App">
-      <IconBar />
-      <div className="form-container">
-        <Form prompt={prompt} setPrompt={setPrompt} handleSubmit={handleSubmit} settings={settings} setSettings={setSettings} />
-      </div>
-      <div style={isLoading ? { overflow: 'hidden' } : {}} className='responses'>
-        {!responses.length && !isLoading && <h2>Ask GPT3 anything!</h2>}
-        {transitionIcon((style, loading) => (
-          loading ? <animated.div style={style}>
-            <LoadingIcon />
-          </animated.div> : null
-        ))}
-        {transitionCard((style, loaded) => (
-          loaded ? responses.map((response, index) => (
-            <animated.div className='card' key={index} style={style}>
-              <Card response={response} />
-              <small style={{ position: 'absolute', right: 0, bottom: 0, float: 'right' }}>{response.model}</small>
-            </animated.div>
-          ))
-            : null))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<IconBar />
+			<div className="form-container">
+				<Form
+					prompt={prompt}
+					setPrompt={setPrompt}
+					handleSubmit={handleSubmit}
+					settings={settings}
+					setSettings={setSettings}
+				/>
+			</div>
+			<div
+				style={isLoading ? { overflow: "hidden" } : {}}
+				className="responses"
+			>
+				{!responses.length && !isLoading && <h2>Ask GPT3 anything!</h2>}
+				{transitionIcon((style, loading) =>
+					loading ? (
+						<animated.div style={style}>
+							<LoadingIcon />
+						</animated.div>
+					) : null
+				)}
+				{transitionCard((style, loaded) =>
+					loaded
+						? responses.map((response, index) => (
+								<animated.div className="card" key={index} style={style}>
+									<Card response={response} />
+									<small
+										style={{
+											position: "absolute",
+											right: 0,
+											bottom: 0,
+											float: "right",
+										}}
+									>
+										{response.model}
+									</small>
+								</animated.div>
+						  ))
+						: null
+				)}
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,63 +1,104 @@
-import React, { useState, useRef } from 'react'
-import { useTransition, animated } from 'react-spring'
+import React, { useState, useRef } from "react"
+import { useTransition, animated } from "react-spring"
 
 export default function Form(props) {
-    const [open, setOpen] = useState(false)
-    const animateDetails = useTransition(open, {
-        from: { opacity: 0, maxHeight: '0' },
-        enter: { opacity: 1, maxHeight: '300px' },
-        leave: { opacity: 0, maxHeight: '0' }
-    })
+	const [open, setOpen] = useState(false)
+	const animateDetails = useTransition(open, {
+		from: { opacity: 0, maxHeight: "0" },
+		enter: { opacity: 1, maxHeight: "300px" },
+		leave: { opacity: 0, maxHeight: "0" },
+	})
 
-    const handleSummaryOpen = (e) => {
-        e.preventDefault()
-        setOpen(!open)
-    }
+	const handleSummaryOpen = (e) => {
+		e.preventDefault()
+		setOpen(!open)
+	}
 
-    const handleSetPrompt = (e) => {
-        props.setPrompt(e.target.value)
-    }
+	const handleSetPrompt = (e) => {
+		props.setPrompt(e.target.value)
+	}
 
-    const handleSetSettings = (e) => {
-        props.setSettings(state => ({ ...state, [e.target.name]: e.target.value }));
-    }
+	const handleSetSettings = (e) => {
+		props.setSettings((state) => ({
+			...state,
+			[e.target.name]: e.target.value,
+		}))
+	}
 
-    return (
-        <>
-            <h1>Ask GPT-3</h1>
-            <form onSubmit={props.handleSubmit}>
-                <textarea autoFocus placeholder='Write prompt here...' onChange={handleSetPrompt} value={props.prompt} name="prompt" id="text" cols="30" rows="4"></textarea>
-                <details open>
-                    <summary onClick={handleSummaryOpen} >{open ? "Model" : "Settings"}</summary>
-                    {animateDetails((style, isOpen) => (
-                        isOpen ? <animated.div style={style}>
-                            <div className="detailsContent">
+	return (
+		<>
+			<h1>Ask GPT-3</h1>
+			<form onSubmit={props.handleSubmit}>
+				<textarea
+					autoFocus
+					placeholder="Write prompt here..."
+					onChange={handleSetPrompt}
+					value={props.prompt}
+					name="prompt"
+					id="text"
+					cols="30"
+					rows="4"
+				></textarea>
+				<details open>
+					<summary onClick={handleSummaryOpen}>
+						{open ? "Model" : "Settings"}
+					</summary>
+					{animateDetails((style, isOpen) =>
+						isOpen ? (
+							<animated.div style={style}>
+								<div className="detailsContent">
+									<select
+										onChange={handleSetSettings}
+										value={props.settings.model}
+										name="model"
+									>
+										<option disabled selected hidden>
+											Pick an engine..
+										</option>
+										<option value="text-curie-001">
+											text-curie-001 (default)
+										</option>
+										<option value="text-ada-001">text-ada-001 (faster)</option>
+										<option value="text-davinci-002">
+											text-davinci-002 (more sophisticated)
+										</option>
+									</select>
+									<hr />
 
-                                <select onChange={handleSetSettings} value={props.settings.model} name="model" >
-                                    <option disabled selected hidden >Pick an engine..</option>
-                                    <option value='text-curie-001'>text-curie-001 (default)</option>
-                                    <option value='text-ada-001'>text-ada-001 (faster)</option>
-                                    <option value='text-davinci-002'>text-davinci-002 (more sophisticated)</option>
-                                </select>
-                                <hr />
+									<label htmlFor="temperature">Temperature (creativity)</label>
+									<input
+										onChange={handleSetSettings}
+										value={props.settings.temperature}
+										type="range"
+										name="temperature"
+										defaultValue={0.5}
+										step={0.01}
+										min={0}
+										max={1}
+									/>
+									<hr />
 
-                                <label htmlFor="temperature">Temperature (creativity)</label>
-                                <input onChange={handleSetSettings} value={props.settings.temperature} type="range" name="temperature" defaultValue={0.5} step={0.01} min={0} max={1} />
-                                <hr />
-
-                                <label htmlFor="maxTokens">Max tokens (response length)</label>
-                                <input onChange={handleSetSettings} value={props.settings.maxTokens} type="range" name="max_tokens" min={20} max={200} />
-                            </div>
-                        </animated.div>
-                            : null
-                    ))}
-
-                </details>
-                <button onClick={() => setOpen(false)} type="submit" >Submit</button>
-                {/* Focus textarea after submit */}
-            </form>
-        </>
-
-
-    )
+									<label htmlFor="maxTokens">
+										Max tokens (response length)
+									</label>
+									<input
+										onChange={handleSetSettings}
+										value={props.settings.max_tokens}
+										type="range"
+										name="max_tokens"
+										min={20}
+										max={200}
+									/>
+								</div>
+							</animated.div>
+						) : null
+					)}
+				</details>
+				<button onClick={() => setOpen(false)} type="submit">
+					Submit
+				</button>
+				{/* Focus textarea after submit */}
+			</form>
+		</>
+	)
 }
