8bb5c1213e8a9086ea0ef2352dccc51f33bbb057
moshawa <moshawa0@gmail.com>
2023-10-07 19:37:49 -0400

    update icon bar

--- src/components/IconBar/IconBar.jsx
+++ src/components/IconBar/IconBar.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { FaLinkedin, FaGithubSquare } from 'react-icons/fa'
+import React from "react"
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa"
 
 export default function IconBar() {
-    return (
-        <div className='iconBar'>
-            <div>
-                <a href="http://linkedin.com/in/mo-shawa">
-                    <FaLinkedin size={40} />
-                </a>
-                <a href="http://github.com/mo-shawa">
-                    <FaGithubSquare size={40} />
-                </a>
-            </div>
-            <a href='http://moshawa.com'>moshawa.com</a>
-        </div>
-    )
+	return (
+		<div className="iconBar">
+			<div>
+				<a href="http://linkedin.com/in/mo-shawa">
+					<FaLinkedin size={40} />
+				</a>
+				<a href="http://github.com/mo-shawa">
+					<FaGithubSquare size={40} />
+				</a>
+			</div>
+			<a href="http://shawa.dev">shawa.dev</a>
+		</div>
+	)
 }
